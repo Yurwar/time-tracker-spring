f574c38195c27608bc448c15ff608b23038d853c
Yurii Matora <yura.matora11@gmail.com>
2019-08-01 14:37:56 +0300

    fix: remove unnecessary java script folder

--- src/main/resources/public/js/usersListApp.js
+++
@@ -1,8 +0,0 @@
-let usersListApp= angular.module("usersListApp", []);
-
-usersListApp.controller("usersController", function ($scope, $http) {
-    $scope.users = [];
-    $http.get('api/user').then(function (response) {
-        $scope.users = response.data;
-    })
-});
